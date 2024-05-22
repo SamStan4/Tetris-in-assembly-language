f8481a7ab88de16566ae7648acf82dc22eee041a
samStan4 <sam.charles.stanley@gmail.com>
2024-05-21 18:25:38 -0700

    more shell stuff

--- build_code.sh
+++ build_code.sh
@@ -2,17 +2,22 @@
 
 object_file="tetris.o"
 executable_file="tetris"
+
 if [ -e "$object_file" ]; then
     rm "$object_file"
 fi
+
 if [ -e "$executable_file" ]; then
     rm "$executable_file"
 fi
+
 if ! command -v nasm >/dev/null 2>&1; then
     echo "please install nasm"
     exit 1
 fi
+
 nasm -f elf64 -o tetris.o tetris.asm
+
 if [ -e "$object_file" ]; then
     ld -o tetris tetris.o
 fi
