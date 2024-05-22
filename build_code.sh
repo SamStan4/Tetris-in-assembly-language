a9627f68ef1d81b84c99ffe23452d8423cbbe7e3
samStan4 <sam.charles.stanley@gmail.com>
2024-05-21 18:23:17 -0700

    updated build script

--- build_code.sh
+++ build_code.sh
@@ -8,6 +8,10 @@ fi
 if [ -e "$executable_file" ]; then
     rm "$executable_file"
 fi
+if ! command -v nasm >/dev/null 2>&1; then
+    echo "please install nasm"
+    exit 1
+fi
 nasm -f elf64 -o tetris.o tetris.asm
 if [ -e "$object_file" ]; then
     ld -o tetris tetris.o
